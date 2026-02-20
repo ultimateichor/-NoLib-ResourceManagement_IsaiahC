b409bf883f9674ce9fb36ab9cbbf579547ae08d5
ultimateichor <icarr7@my.gcu.edu>
2026-02-20 09:49:01 -0700

    Fix repository links and enhance run instructions
    Updated repository links and improved run instructions.

--- Assets/README/README.cs
+++ Assets/README/README.cs
@@ -1,14 +1,16 @@
 // thank you for reading me :)
 
-GitHub Repository:
+GitHub Repositories:
 https://github.com/ultimateichor/-NoLib-ResourceManagement_IsaiahC
+https://github.com/ultimateichor/-NoLib-ObjectPooling_IsaiahC
 
 Run instructions:
 
-Simply build the project and run in Unity. The program will demonstrate various memory management techniques in C#, including garbage collection, 
+Simply build the projects and run in Unity. The program will demonstrate various memory management techniques in C#, including garbage collection, 
     memory allocation, and deallocation.
 
-Library is not present; it will be rebuilt automatically when you open the project in Unity. No additional setup is required.
+Library is not present; it will be rebuilt automatically when you open the project in Unity (this is why there is a
+    loading delay). No additional setup is required.
 
 How to replicate:
     - Clone the repository to your local machine using the command: git clone
